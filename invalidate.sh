64f5cd6ffddb13307460494bd2aed89cce8a56d6
Erik Desjardins <erikdesjardins@users.noreply.github.com>
2017-08-15 00:03:46 -0400

    hide curl stats

--- invalidate.sh
+++ invalidate.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-curl -X DELETE "https://api.cloudflare.com/client/v4/zones/$CLOUDFLARE_ZONE/purge_cache" \
+curl -sS -X DELETE "https://api.cloudflare.com/client/v4/zones/$CLOUDFLARE_ZONE/purge_cache" \
   -H "X-Auth-Email: $CLOUDFLARE_EMAIL" \
   -H "X-Auth-Key: $CLOUDFLARE_API_KEY" \
   -H "Content-Type: application/json" \
